39a8b504734c7ce0640f56fd2bcad000f664b2c5
Jan Brecka <jan.brecka@gmail.com>
2022-08-01 10:17:06 +0200

    wip

---
+++ src/recoil.ts
@@ -0,0 +1,23 @@
+export function atom() {}
+
+export function atomFamily() {}
+
+export function selector() {}
+
+export function selectorFamily() {}
+
+export function useRecoilCallback() {}
+
+export function useRecoilTransaction() {}
+
+export function useRecoilValue() {}
+
+export function useRecoilValueLoadable() {}
+
+export function useRecoilState() {}
+
+export function useSetRecoilState() {}
+
+export function waitForAll() {}
+
+// TODO Loadable
