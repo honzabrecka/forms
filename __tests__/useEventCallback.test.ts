29546ac28ab6850dea6e994d377d69daaad741fe
Jan Brecka <jan.brecka@gmail.com>
2023-02-02 11:56:52 +0100

    improve test

--- __tests__/useEventCallback.test.ts
+++ __tests__/useEventCallback.test.ts
@@ -2,13 +2,15 @@ import { renderHook } from '@testing-library/react';
 import { useEventCallback } from '../src/internalHooks';
 
 test('useEventCallback', () => {
-  const { result, rerender } = renderHook(() =>
-    useEventCallback((x: number) => x + 1),
+  const { result, rerender } = renderHook(
+    ({ y }) => useEventCallback((x: number) => y + x + 1),
+    { initialProps: { y: 0 } },
   );
   const ref = result.current;
-  rerender();
+  rerender({ y: 10 });
   // ref is stable even though passed fn is not
   expect(result.current).toBe(ref);
   // pass args and returns result of inner cb
-  expect(result.current(2)).toBe(3);
+  // and inner cb uses latest props (is not stale)
+  expect(result.current(2)).toBe(13);
 });
